591078280e2d71f9f4ce2a00397f21a1fd7da72a
Sakshi Chandel <schandel@ippon.fr>
2023-06-06 23:24:48 +1000

    Updated

--- demoapp/next.config.js
+++ demoapp/next.config.js
@@ -8,14 +8,16 @@ module.exports = {
   reactStrictMode: true,
   webpack: function (config, options) {
     return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/products",
-        permanent: true,
-      },
-    ];
-  },
-};
+  }
+
+//   },
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/products",
+//         permanent: true,
+//       },
+//     ];
+//   },
+ };
---
+++ demoapp/pages/index.js
@@ -0,0 +1,11 @@
+import { Layout } from "components/Layout";
+
+function Home() {
+    return (
+      <Layout>
+        <h1>Hello</h1>
+      </Layout>
+    );
+  }
+  export default Home;
+  
